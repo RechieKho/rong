91bff800957bbd5c873d27de17dcdbb2f96f45d7
RechieKho <the.real.richie.kho@gmail.com>
2024-03-20 12:55:12 +0800

    Setup project.

---
+++ core/includes/add.hpp
@@ -0,0 +1,6 @@
+#ifndef CORE_ADD_HPP
+#define CORE_ADD_HPP
+
+int add(int p_a, int p_b);
+
+#endif//CORE_ADD_HPP
---
+++ core/sources/add.cpp
@@ -0,0 +1,5 @@
+#include<add.hpp>
+
+int add(int p_a, int p_b){
+    return p_a + p_b;
+}
