415962b1a18ddc085e837f64d53f9f3e74c220a6
RechieKho <the.real.richie.kho@gmail.com>
2024-04-17 20:00:13 +0800

    Remove unnecessary code in `tools`. Update `README.md`.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rong
 
-
+This is yet another ambitious `C++` project in which writing everything from scratch,
+the only twist is that it embrace modern `C++` features (excluding `STL`).
--- tools/hello_world.cpp
+++ tools/hello_world.cpp
@@ -5,31 +5,7 @@ using namespace Rong;
 
 int main()
 {
-    constexpr auto view = ListView<C>("Hello world", 11);
-    constexpr auto sliced = view.slice(2, 6);
-    sliced.for_each([](const C &c)
-                    { fputc(c, stdout); });
-    fputc('\n', stdout);
-
-    auto list = List<C>(sliced);
-    list.prepend('H');
-    list.for_each([](const C &c)
-                  { fputc(c, stdout); });
-    fputc('\n', stdout);
-
-    auto mapped = list.map<C>([](const C &c)
-                              { return 'E'; });
-    mapped.for_each([](const C &c)
-                    { fputc(c, stdout); });
-    fputc('\n', stdout);
-
-    auto filtered = view.filter([](const C &c)
-                                { return c != 'l'; });
-    filtered.for_each([](const C &c)
-                      { fputc(c, stdout); });
-    fputc('\n', stdout);
-
-    constexpr auto hello = ListView<C>("Hello", 5);
+    constexpr auto hello = ListView<C>("Hello ", 6);
     constexpr auto world = ListView<C>("world", 5);
 
     hello.concat(world).for_each([](const C &c)
